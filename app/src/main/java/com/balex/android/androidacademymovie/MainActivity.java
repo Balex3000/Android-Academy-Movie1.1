640ac9b148917aed131450de197b7562305a8f73
alexba <balex3000@hotmail.com>
2018-11-03 16:52:03 +0200

    added movie trailer button with code

--- app/src/main/java/com/balex/android/androidacademymovie/MainActivity.java
+++ app/src/main/java/com/balex/android/androidacademymovie/MainActivity.java
@@ -1,7 +1,10 @@
 package com.balex.android.androidacademymovie;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void playTrailer(View view) {
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=6ZfuNTqbHE8"));
+        startActivity(webIntent);
     }
 }
